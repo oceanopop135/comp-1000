ed6e187e95a18359fc6b4793c5641b3403a7cae5
oceanopop135 <137225924+oceanopop135@users.noreply.github.com>
2025-12-05 11:31:27 +0000

    Add files via upload

---
+++ funtions.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+#include <cstring>
+
+
+int addTwoNumbers(int a, int b){
+    return a + b;
+}
+
+
+
+int main() {
+    
+addTwoNumbers(12, 21);
+
+return 0;
+}
